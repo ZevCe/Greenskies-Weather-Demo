05e5d2481c833caf092dd1d0cde70ce667dd3b30
ZevCe <cellaz@rpi.edu>
2024-08-22 14:42:55 -0400

    Refactor of JS code
    the json is now stored in a global variable which fixes the slight rounding errors that would happen when converting from imperial to metric and grabbing the rounded data from the html. This also means we no longer need a separate function for updating units so now the unit functions just use updateDisplay. Additional layout changes + deleting now unnecessary functions.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //generic unit conversion functions
-function convertToFarenheit(num) {return Math.round((num * (9/5)) + 32);}
 function convertToCelcius(num) {return Math.round((num - 32) * (5/9));}
-function convertToMPH(num) {return Math.round(num/1.609);}
 function convertToKPH(num) {return Math.round(num * 1.609);}
+
+//global variable to keep track of our loaded json
+let results = null;
 
 //grabs all weather data for a location based on a zip code
 function findZip() {
@@ -29,12 +30,13 @@ function findZip() {
             //printing out our json object to the console for easy reference
             console.log(response);
             //updating our divs
-            updateDisplay(response);
+            results = response;
+            updateDisplay();
             setResultDivs("result");
         });
 }
 
-//small helper functions to set which divs are visible when a user submits a search
+//helper functions to set which divs are visible when a user submits a search
 function setResultDivs(status) {
     
     //accessing zipErrorElement and determining if we need to show/hide
@@ -63,30 +65,33 @@ function setResultDivs(status) {
     }
 }
 
-//helper function for updating the icons in our display
-//need to add parameters to check for nighttime at somepoint
-function updateIcon(element, conditions, isNighttime) {
-    //if else statement for determinging which icon to use
-    //would be a switch case but we aren't looking for exact matching
-    if(conditions.includes("Rain")) element.src = "./assets/rainy.png";
-    else if(conditions.includes("Overcast")) element.src = "./assets/cloudy.png";
-    else if(conditions.includes("Partially cloudy")) {
-        if(isNighttime) element.src = "./assets/partlyCloudyNight.png";
-        else element.src = "./assets/partlyCloudy.png";
-    }
-    else if(conditions.includes("Clear")) {
-        if(isNighttime) element.src = "./assets/nighty.png"
-        else element.src = "./assets/sunny.png";
-    }
-    else {
-        element.src = "./assets/snowy.png";
-        console.log(conditions);
-    }
+//bool which keeps track of the current system to display units in
+let isImperial = true;
+
+//changing the units to imperial
+function setImperial() {
+    //changing the variable
+    if(isImperial) return;
+    isImperial = true;
+
+    //checking we have data loaded in before making call to update units
+    if(results != null) updateDisplay();
 }
+
+//changing the units to metric
+function setMetric() {
+    //changing the variable
+    if(!isImperial) return;
+    isImperial = false;
+
+    //checking we have data loaded in before making call to update units
+    if(results != null) updateDisplay();
+}
+
 
 //updating all the elements of our display based on weather data returned from an api call
 //need to check for imperial/metric
-function updateDisplay(results) {
+function updateDisplay() {
     
     //creating a date object that is... up to date
     //...I'm so funny please hire me...
@@ -185,74 +190,25 @@ function updateDisplay(results) {
     }
 }
 
-//bool which keeps track of the current system to display units in
-let isImperial = true;
-
-//changing the units to imperial
-function setImperial() {
-    if(isImperial) return;
-    isImperial = true;
-    setUnit();
-}
-
-//changing the units to metric
-function setMetric() {
-    if(!isImperial) return;
-    isImperial = false;
-    setUnit();
-}
-
-//going through the elements and updating all values to imperial/metric
-function setUnit() {
-
-    //iterating through our hour display
-    for(let i = 1; i < 13; i++) {
-        //accessing the hour element
-        let hourInfo = document.getElementById("hourData" + i);
-
-        //accessing the temp of the current hour
-        let hourTemp = hourInfo.getElementsByClassName("hourTemp")[0];
-        
-        //grabbing just the number of the hour temp for conversion calculations
-        let degree = Number(hourTemp.innerHTML.slice(0, hourTemp.innerHTML.length-1));
-
-        //updating the element with the new number
-        if(isImperial) hourTemp.innerHTML = convertToFarenheit(degree) + "\u00B0";
-        else hourTemp.innerHTML = convertToCelcius(degree) + "\u00B0";
-
-        //grabbing the wind speed text for the current hourly element
-        let windSpeedText = hourInfo.getElementsByClassName("hourWindDiv")[0].getElementsByClassName("hourWindText")[0];
-        //grabbing the number itself from the element
-        let speed = Number(windSpeedText.innerHTML.slice(0, windSpeedText.innerHTML.length-3));
-
-        //updating the element with the new units
-        if(isImperial) windSpeedText.innerHTML = convertToMPH(speed) + " mph";
-        else windSpeedText.innerHTML = convertToKPH(speed) + " kph";
+//helper function for updating the icons in our display
+//need to add parameters to check for nighttime at somepoint
+function updateIcon(element, conditions, isNighttime) {
+    //if else statement for determinging which icon to use
+    //would be a switch case but we aren't looking for exact matching
+    if(conditions.includes("Rain")) element.src = "./assets/rainy.png";
+    else if(conditions.includes("Overcast")) element.src = "./assets/cloudy.png";
+    else if(conditions.includes("Partially cloudy")) {
+        if(isNighttime) element.src = "./assets/partlyCloudyNight.png";
+        else element.src = "./assets/partlyCloudy.png";
     }
-
-    //iterating through our day display
-    for(let i = 1; i < 8; i++) {
-        //accessing the day element
-        let dayInfo = document.getElementById("dayData" + i);
-
-        //accessing the div containing the high and low text elements for the day
-        let highLowContainer = dayInfo.getElementsByClassName("highLowContainer")[0];
-
-        //accessing the high text from the div
-        let high = highLowContainer.getElementsByClassName("high")[0];
-        //accessing just the number of the text for calculations
-        let degree = Number(high.innerHTML.slice(2, high.innerHTML.length-1));
-        //updating the element with the text
-        if(isImperial) high.innerHTML = "H: " + convertToFarenheit(degree) + "\u00B0";
-        else high.innerHTML = "H: " + convertToCelcius(degree) + "\u00B0"; 
-
-        //accessing the high text from the div
-        let low = highLowContainer.getElementsByClassName("low")[0];
-        //accessing just the number of the text for calculations
-        degree = Number(low.innerHTML.slice(2, low.innerHTML.length-1));
-        //updating the element with the text
-        if(isImperial) low.innerHTML = "L: " + convertToFarenheit(degree) + "\u00B0";
-        else low.innerHTML = "L: " + convertToCelcius(degree) + "\u00B0";
+    else if(conditions.includes("Clear")) {
+        if(isNighttime) element.src = "./assets/nighty.png"
+        else element.src = "./assets/sunny.png";
+    }
+    else {
+        //for now just assuming unknown conditions are snowy
+        element.src = "./assets/snowy.png";
+        console.log(conditions);
     }
 }
 
